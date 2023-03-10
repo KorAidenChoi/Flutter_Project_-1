92066e37527a2d04b06520703d86d56ebb474009
KorAidenChoi <kor.aiden.choi@gmail.com>
2023-03-09 19:53:45 -0800

    quick fix guideline code actions

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ void main() {
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +15,6 @@ class App extends StatelessWidget {
       body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 40),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(
                 height: 80,
@@ -37,95 +38,13 @@ class App extends StatelessWidget {
                           color: Colors.white.withOpacity(0.8),
                           fontSize: 22,
                         ),
-                      ),
+                      )
                     ],
                   )
                 ],
-              ),
-              const SizedBox(
-                height: 120,
-              ),
-              Text(
-                "Total Balance",
-                style: TextStyle(
-                  color: Colors.white.withOpacity(0.8),
-                  fontSize: 22,
-                ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              const Text(
-                "\$5 194 482",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 44,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.circular(45)),
-                    child: const Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 40),
-                      child: Text(
-                        "Transfer",
-                        style: TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              )
             ],
           )),
     ));
   }
 }
-  // @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //       home: Scaffold(
-  //     appBar: AppBar(
-  //       title: const Text(
-  //         "Shop",
-  //         style: TextStyle(fontSize: 40),
-  //       ),
-  //       backgroundColor: Colors.black,
-  //       centerTitle: true,
-  //     ),
-  //     body: Padding(
-  //       padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 80),
-  //       child: Column(
-  //         children: [
-  //           Row(
-  //             children: [
-  //               Column(
-  //                 children: [
-  //                   Container(
-  //                     padding: const EdgeInsets.all(20),
-  //                     decoration: BoxDecoration(
-  //                         border: Border.all(
-  //                             color: const Color.fromARGB(255, 51, 112, 255))),
-  //                     child: const Text(
-  //                       'Mangoes',
-  //                       style: TextStyle(fontSize: 25),
-  //                     ),
-  //                   )
-  //                 ],
-  //               ),
-  //             ],
-  //           ),
-  //         ],
-  //       ),
-  //     ),
-  //   ));
-
